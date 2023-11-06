e14374ec4a5bb34245bc84259d7fbfa21036c984
lht2348 <gusxo1028@naver.com>
2023-11-06 22:49:58 +0900

    ex

--- project1/src/project1/project12.java
+++
@@ -1,9 +0,0 @@
-package project1;
-
-public class project12 {
-	public static void main(String[] args) {
-		System.out.print("hello");
-		System.out.println("hello??");
-		System.out.println("hello??");
-	}
-}
